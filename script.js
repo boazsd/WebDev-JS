bed836fcc311c0d1bbfc52e6140f9a4d5ca82366
boazsd <boazsd@gmail.com>
2017-07-05 09:19:43 +0300

    initial trargil 1-3

---
+++ script.js
@@ -0,0 +1,18 @@
+/*Tar 2 */
+var num1 = 11;
+var num2 = 22;
+if (num1 > num2) {
+    console.log("Num1 " + num1);
+} else {
+    console.log("Num2 " + num2);
+}
+
+/*Tar 3 */
+var num1 = 11;
+var num2 = 22;
+var num3 = -32;
+if (num1 + num2 + num3 > 0) {
+    console.log("The sign is: +");
+} else {
+    console.log("The sign is: -");
+}
