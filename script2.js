5a103c837f45d8f847f6e664ba38a9cfd97d394e
boazsd <boazsd@gmail.com>
2017-07-06 13:31:20 +0300

    update

--- script2.js
+++ script2.js
@@ -10,3 +10,17 @@ function myReverse ( myString) {
     return temp;
 }
 
+/*Tar 2 */
+function myPalindrome( myString) {
+    var temp = myString.replace(/ /g, "");
+    var isPalindrome = true;
+    var myLength = temp.length;
+    var i=0;
+    while (isPalindrome && (i < myLength/2)) {
+        isPalindrome = (temp[i] === temp[myLength - i - 1]);
+       console.log(i + temp[i] + " - " + temp[myLength - i - 1]);
+       i++;
+     }
+    return isPalindrome ? "Palindrome" : "Not Palindrome";
+}
+
