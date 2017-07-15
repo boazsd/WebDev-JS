ee5e9aaac2480aef647d45ee64b6b7ba228a56e3
boazsd <boazsd@gmail.com>
2017-07-16 00:14:11 +0300

    CodeSchool updat1

--- CodeSchool/Level1/app1.js
+++ CodeSchool/Level1/app1.js
@@ -1,5 +1,9 @@
 (function(){
-  var gem = { name: 'Azurite', price: 2.95 };
+  var gem = [{ name: 'Azurite', price: 2.95 },
+  { name: 'Boaz', price: 3.95 },
+{ name: 'Shirley', price: 4.95 },
+{ name: 'Roy', price: 5.95 },
+{ name: 'Eitan', price: 6.95 }];
   var app = angular.module('gemStore', []);
   app.controller('StoreController', function () {
     this.product = gem;
