0a013721c3349a853f1226ab8c64c0be2a0014df
boazsd <boazsd@gmail.com>
2017-07-16 13:47:15 +0300

    Update Angular

--- Angular/app1.js
+++ Angular/app1.js
@@ -1,23 +1,34 @@
-var ActorApp = angular.module("ActorApp", []);
+var ActorApp = angular.module("actorsApp", []);
 ActorApp.controller("ActorCtrl", function ($scope) {
 
-$scope.actor(first, last, img, birthDate, nationality, aUrl) {
-    this.name.firstName = first;
-    this.name.lastName = last;
-    this.actorImg = img;
-    this.actorNationality = nationality;
-    this.actorURL = aUrl;
-}
-  /*  
+  function actor(first, last, img, birthDate, aCountry, aUrl) {
+      this.name.firstName = first;
+      this.name.lastName = last;
+      this.actorImg = img;
+      this.birthDate = birthDate;
+      this.Country = aCountry;
+      this.actorURL = aUrl;
+      console.log(this);
+  }
+
+  $scope.actors = [
+    new actor("Gal", "Gadot", "GalGadot-Img.jpg", "01/01/01", "Israel", "http://www.imdb.com/name/nm2933757/"),
+    new actor("Gal", "Gadot", "", "", "", "" ),
+    new actor("Robert", "De Niro", "", "", "", "" ),
+    new actor("Leonardo", "DiCaprio", "", "", "", "" ),
+    new actor("Angelina", "Jolie", "", "", "", "" ),
+    new actor("Brad", "Pitt", "", "", "", "" ),
+    new actor("Julia", "Roberts", "", "", "", "" )
+  ];
+ });
+
+
+ /*  
   $http.get("actors.html").success(function(response) {
     $scope.actors = response.data;
   }).error(function(data, status, headers, config) {
     console.log("Error");
   });
-*/
-});
-
-
 
 scope.actors = {
 {"name": {"firstName": "Gal", "lastName": "Gadot"}, "actorImg": "", "birthDate": "", "actorNationality": "", "actorURL": "" },
@@ -31,3 +42,4 @@ scope.actors = {
 {"name": {"firstName": "Gal8", "lastName": "bGadot"}, "actorImg": "", "birthDate": "", "actorNationality": "", "actorURL": "" },
 {"name": {"firstName": "Gal9", "lastName": "aGadot"}, "actorImg": "", "birthDate": "", "actorNationality": "", "actorURL": "" }
 };
+*/
