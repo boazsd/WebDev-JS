53f24bf93f81defe4daa852b860132bc636b8510
boazsd <boazsd@gmail.com>
2017-07-17 05:17:36 +0300

    update the actors view items

--- Angular/app1.js
+++ Angular/app1.js
@@ -1,9 +1,8 @@
-var ActorApp = angular.module("actorsApp", []);
-ActorApp.controller("ActorCtrl", function ($scope) {
+var actorApp = angular.module("actorsApp", []);
+actorApp.controller("actorCtrl", function ($scope) {
 
   function actor(first, last, img, birthDate, aCountry, aUrl) {
-      this.name.firstName = first;
-      this.name.lastName = last;
+      this.name= {firstName: first,lastName:last};
       this.actorImg = img;
       this.birthDate = birthDate;
       this.Country = aCountry;
@@ -12,19 +11,20 @@ ActorApp.controller("ActorCtrl", function ($scope) {
   }
 
   $scope.actors = [
-    new actor("Gal", "Gadot", "GalGadot-Img.jpg", "01/01/01", "Israel", "http://www.imdb.com/name/nm2933757/"),
-    new actor("Gal", "Gadot", "", "", "", "" ),
-    new actor("Robert", "De Niro", "", "", "", "" ),
-    new actor("Leonardo", "DiCaprio", "", "", "", "" ),
-    new actor("Angelina", "Jolie", "", "", "", "" ),
-    new actor("Brad", "Pitt", "", "", "", "" ),
-    new actor("Julia", "Roberts", "", "", "", "" )
+    new actor("Gal", "Gadot", "GalGadot-Img.jpg", "April 30, 1985", "Rosh Ha'ayin, Israel", "http://www.imdb.com/name/nm2933757/"),
+    new actor("Robert", "De Niro", "RobertDeNiro-Img.jpg", "August 17, 1943", "New York City, New York, USA", "http://www.imdb.com/name/nm0000134/" ),
+    new actor("Angelina", "Jolie", "AngelinaJolie-Img.jpg", "June 4, 1975", "Los Angeles, California, USA", "http://www.imdb.com/name/nm0001401/" ),
+    new actor("Leonardo", "DiCaprio", "LeonardoDiCaprio-Img.jpg", "November 11, 1974", "Hollywood, Los Angeles, California, USA", "http://www.imdb.com/name/nm0000138/" ),
+    new actor("Brad", "Pitt", "BradPitt-Img.jpg", "December 18, 1963", "Shawnee, Oklahoma, USA", "http://www.imdb.com/name/nm0000093/" ),
+    new actor("Bruce", "Willis", "BruceWillis-Img.jpg", "March 19, 1955", "Idar-Oberstein, West Germany", "http://www.imdb.com/name/nm0000246/" ),
+    new actor("Tom", "Cruise", "TomCruise-Img.jpg", "July 3, 1962", "Syracuse, New York, USA", "//http://www.imdb.com/name/nm0000129/" ),
+    new actor("Julia", "Roberts", "JuliaRoberts-Img.jpg", "October 28, 1967", "Smyrna, Georgia, USA", "http://www.imdb.com/name/nm0000210/" )
   ];
  });
 
 
  /*  
-  $http.get("actors.html").success(function(response) {
+  $http.get("actors.json").success(function(response) {
     $scope.actors = response.data;
   }).error(function(data, status, headers, config) {
     console.log("Error");
---
+++ Coverflow/script.js
@@ -0,0 +1,176 @@
+/**
+ * Sets a request for the next animation frame
+ */
+
+var renderTimeout = -1;
+
+window.requestAnimFrame = (function() {
+  return window.requestAnimationFrame
+      || window.webkitRequestAnimationFrame
+      || window.mozRequestAnimationFrame
+      || window.oRequestAnimationFrame
+      || window.msRequestAnimationFrame
+      || function(callback) {
+            renderTimeout = window.setTimeout(callback, 1000 / 60);
+      };
+}());
+
+/**
+ * Cancels a request for a scheduled repaint of the window for the next animation frame
+ */
+window.cancelAnimFrame = (function(){
+    return window.cancelAnimationFrame
+        || window.webkitCancelRequestAnimationFrame
+        || window.mozCancelRequestAnimationFrame
+        || window.oCancelRequestAnimationFrame
+        || window.msCancelRequestAnimationFrame
+        || window.clearTimeout(renderTimeout);
+}());
+
+
+/**
+ *  Creates OS X style coverflow effect using CSS3 styles and transforms
+ *
+ *  Inspired by Hakim's (@hakimel) Scroll Effects demo:
+ *  http://lab.hakim.se/scroll-effects/
+ */
+var coverflow = {
+
+	scrWrapper: null,
+
+	imageClickHandler: function(event) {
+		//TODO implement auto scrolling to the clicked image
+	},
+
+
+	bind: function(coverflowDOM) {
+
+		var list = coverflowDOM.querySelector('ul');
+
+		// Create separate div for a scroller bar
+		var scrollerWrapper = document.createElement('div');
+		scrollerWrapper.classList.add('scroller-wrapper');
+
+		scrWrapper = scrollerWrapper;
+
+		// And add the scroller content
+		var scroller = document.createElement('div');
+		scroller.classList.add('scroller');
+		// Scroller width has to be the same as the width of the list
+		scroller.style.width = list.scrollWidth + 'px';
+
+		scrollerWrapper.appendChild(scroller);
+		coverflowDOM.appendChild(scrollerWrapper);
+
+		var scrollTarget = 1;
+		var scrollSnapped = 0;
+		var item = null;
+		var itemOffset = null;
+		var normOffset = null;
+		var opacity = null;
+		var itemClass = null;
+		var scrollLeft = null;
+		var currentId = null;
+		var delta = 1;
+
+		var items = Array.prototype.slice.apply(list.children);
+		var viewWidth = list.offsetWidth;
+		var halfViewWidth = Math.floor(list.offsetWidth / 2);
+		var numItems = items.length;
+
+		for (var i = 0; i < numItems; i++) {
+			items[i]._offsetLeft = items[i].offsetLeft;
+			items[i].addEventListener('click', this.imageClickHandler, false);
+		}
+
+		var leftMargin = items[0]._offsetLeft;
+
+		return (function() {
+
+			(function animloop() {
+				requestAnimFrame(animloop);
+				update();
+			})();
+
+			function update() {
+				scrollLeft = scrollerWrapper.scrollLeft;
+				// Discretise the scrollbar position
+				scrollSnapped = Math.round(scrollLeft / numItems) * numItems;
+				currentId = Math.round(scrollSnapped / 55);
+				delta = scrollSnapped - scrollTarget;
+				scrollTarget += delta * 0.1;
+				list.style.left = (-scrollTarget) + 'px';
+
+				// Quit when it slows down enough
+				if (Math.abs(delta) > 0.01) {
+
+					for (var i = 0; i < numItems; i++) {
+						item = items[i];
+						itemOffset = item._offsetLeft - leftMargin;
+						normOffset = (itemOffset - scrollTarget) / halfViewWidth;
+						opacity = 1.2 - Math.abs(Math.pow(normOffset / 0.8, 8));
+						item.style.opacity = opacity;
+						itemClass = item.className;
+
+						if (itemClass.length) {
+							item.classList.remove('prev');
+							item.classList.remove('next');
+							item.classList.remove('current');
+						}
+
+						if (currentId > i) {
+							item.classList.add('prev');
+							item.style.zIndex = i;
+						}
+
+						else if (currentId < i) {
+							item.classList.add('next');
+							item.style.zIndex = numItems - i;
+						}
+
+						else {
+							item.classList.add('current');
+							item.style.zIndex = numItems;
+						}
+
+						  //item.innerHTML = i + ':' + opacity;
+					}
+				}
+			}
+		})();
+	}
+};
+
+
+// 50 is sensible
+var numItems = 20;
+var types = [
+  'food',
+  'animals',
+  'abstract',
+  'city',
+  'nightlife',
+  'fashion',
+  'people',
+  'nature',
+  'sports',
+  'technics',
+  'transport'
+];
+
+for (var i = 0, markup = ''; i < numItems; i++)
+{
+  markup += '<li><a href="#"><img data-id="' + i + '" src="http://lorempixel.com/150/150/'
+    + types[Math.floor(Math.random() * types.length)]
+    + '?' + Math.floor(Math.random() * 10000)
+    + '"></a></li>';
+}
+
+			var wrapper = document.createElement('div');
+			wrapper.classList.add('wrapper');
+			wrapper.innerHTML = '<ul>' + markup + '</ul>';
+
+			var coverflowDOM = document.querySelectorAll('.coverflow')[0];
+			coverflowDOM.appendChild(wrapper);
+			coverflow.bind(coverflowDOM);
+
