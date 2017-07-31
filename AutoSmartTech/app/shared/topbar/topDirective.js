85f7901585cd0461be6bf509823a97c712062fa2
boazsd <boazsd@gmail.com>
2017-07-31 18:53:52 +0300

    Top navbar embedded in the main view

--- AutoSmartTech/app/shared/topbar/topDirective.js
+++ AutoSmartTech/app/shared/topbar/topDirective.js
@@ -1,13 +1,12 @@
-var app = angular.module("myApp", []);
-app.directive("w3TestDirective", function() {
+homeTechApp.directive("w3TestDirective", function() {
     return {
         template : "<h1>Made by a directive!</h1>"
     };
 });
 
-app.directive("boazTestDirective", function() {
+homeTechApp.directive("topNavbarDirective", function() {
     return {
-        template : "<h1>Made by a directive!</h1>"
+        template : "<nav class='navbar navbar-inverse' role='navigation'>   <div class='navbar-header'> <a class='' href='#'>   <img class='logo' ng-src='assets/img/smart-home-icon-black-150x150.png'  alt=''>   </a>    <a class='navbar-brand' href='#'>SmartTech@Home</a> <button type='button' class='navbar-toggle' data-toggle='collapse' data-target='.navbar-collapse'>  <span class='icon-bar'></span>  <span class='icon-bar'></span>  <span class='icon-bar'></span>  </button>   </div>  <div class='navbar-collapse collapse'>  <ul class='nav navbar-nav navbar-left'> </ul>   <ul class='nav navbar-nav navbar-center'>   </ul>   <ul class='nav navbar-nav navbar-right'>    <li><a href='#'><i class='fa fa-gear fa-2x'></i></a></li>   <li><a href='#'><i class='fa fa-question-circle-o fa-2x'></i></a></li>  <li><a href='#'><i class='fa fa-power-off fa-2x'></i></a></li>  </ul>   </div>  </nav>"
     };
 });
 
