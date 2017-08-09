dec5dbdd0f9ccdf1676e164317f4f2b5bffb65c7
boazsd <boazsd@gmail.com>
2017-08-09 14:39:08 +0300

    delet

--- AutoSmartTech/app/app.routes.js
+++
@@ -1,9 +0,0 @@
-homeTechApp.controller("NavCtrl", function ($scope, activeUser) {
-    
-    //$scope.isLoggedIn = activeUser.isLoggedIn();
-    
-
-    $scope.isLoggedIn = function() {
-        return activeUser.isLoggedIn();
-    };
-});
